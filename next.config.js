3ffc981a8d68b8035b4ba26b14477f0441a14964
julianp-ampion <jparengo@ampion-businesspartners.net>
2023-12-22 13:03:29 -0300

    Added CSP and x-frame headers.

--- next.config.js
+++ next.config.js
@@ -4,9 +4,83 @@ const { execSync } = require('child_process');
 const { HONEYBADGER_API_KEY, HONEYBADGER_ASSETS_URL, NODE_ENV } = process.env;
 const HONEYBADGER_REVISION = execSync('git rev-parse HEAD').toString().trim();
 
+const ContentSecurityPolicy = `
+  connect-src 'self' 
+  *.honeybadger.io
+  *.ampion.net
+  *.bing.com
+  *.wistia.com
+  *.litix.io
+  *.segment.com
+  *.segment.io
+  *.google-analytics.com
+  *.oribi.io
+  *.doubleclick.net; 
+
+  default-src 'self'; 
+
+  font-src 'self' data:; 
+
+  frame-src 'self' 
+  www.youtube.com
+  www.google.com
+  *.doubleclick.net
+  *.wistia.com; 
+
+  img-src 'self'
+  * data:; 
+
+  media-src 'self' blob:; 
+
+  style-src 'self' 'unsafe-inline'; 
+
+  script-src 
+  'self' 
+  'unsafe-eval' 
+  *.google.com
+  *.gstatic.com
+  *.segment.com
+  blob:; 
+
+  script-src-elem 'self' 'unsafe-inline'
+  *.wistia.com
+  *.segment.com
+  *.google.com
+  *.google-analytics.com
+  *.googleadservices.com
+  *.googletagmanager.com
+  *.doubleclick.net
+  *.gstatic.com
+  *.ads-twitter.com
+  *.facebook.net
+  *.bing.com
+  *.licdn.com
+  *.nextdoor.com
+`;
+
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+    async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
   env: {
     DATOCMS_API_TOKEN: process.env.DATOCMS_API_TOKEN,
     DATOCMS_API_URL: process.env.DATOCMS_API_URL,
